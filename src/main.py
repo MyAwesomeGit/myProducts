4b0892d500704befa206e4b45157e76b9ae0783f
Vasily Shuklin <b88bb@yandex.ru>
2024-04-04 19:07:35 +0500

    Add products data

--- src/main.py
+++ src/main.py
@@ -19,10 +19,3 @@ if __name__ == "__main__":
     uvicorn.run(app,
                 host='127.0.0.1',
                 port=8080)
-
-
-
-
-
-
-
---
+++ src/models/products.py
@@ -0,0 +1,42 @@
+product_1 = {
+    "product_id": 123,
+    "name": "Keyboard",
+    "category": "Electronics",
+    "price": 112.99
+}
+
+product_2 = {
+    "product_id": 456,
+    "name": "Mobile phone",
+    "category": "Electronics",
+    "price": 1100.00
+}
+
+product_3 = {
+    "product_id": 42,
+    "name": "Phone case",
+    "category": "Accessories",
+    "price": 42.99
+}
+
+product_4 = {
+    "product_id": 99,
+    "name": "Iphone data cable",
+    "category": "Accessories",
+    "price": 19.99
+}
+
+product_5 = {
+    "product_id": 2,
+    "name": "Smartwatch",
+    "category": "Electronics",
+    "price": 99.99
+}
+
+products = {
+    1: product_1,
+    2: product_2,
+    3: product_3,
+    4: product_4,
+    5: product_5
+}
