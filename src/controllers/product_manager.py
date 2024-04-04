184be7da9d2ba4cb6d1a6a6957f3a97f3564a4f0
Vasily Shuklin <b88bb@yandex.ru>
2024-04-04 19:21:45 +0500

    Update get_product method

--- src/controllers/product_manager.py
+++ src/controllers/product_manager.py
@@ -1,4 +1,13 @@
+from typing import Optional, Dict, Any
+from fastapi import HTTPException
+
+
 class ProductManager:
+    def __init__(self, products: Optional[Dict[int, Dict[str, Any]]] = None,
+                 product_id: Optional[int] = None):
+        self.products = products
+        self.product_id = product_id
+
     def __init_(self):
         pass
 
@@ -6,7 +15,10 @@ class ProductManager:
         pass
 
     async def get_product(self):
-        pass
+        if not self.product_id:
+            raise HTTPException(status_code=404, detail="Product not found")
+        self.product_info = self.products.get(self.product_id)
+        return self.product_info
 
     async def search_products(self):
         pass
--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
-
+from controllers.product_manager import ProductManager
+from models.products import products
 
 app = FastAPI()
 
 
 @app.get("/product/{product_id}")
-async def get_product():
-    pass
+async def get_product(product_id: int):
+    product_manager = ProductManager(product_id=product_id,
+                                     products=products)
+    return await product_manager.get_product()
 
 
 @app.get("/products_search")
