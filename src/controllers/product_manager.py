72abab65ead3e77cfe2f0bc028814bb0c8e65c66
Vasily Shuklin <b88bb@yandex.ru>
2024-04-04 19:14:24 +0500

    Add ProductManager class

---
+++ src/controllers/product_manager.py
@@ -0,0 +1,12 @@
+class ProductManager:
+    def __init_(self):
+        pass
+
+    async def add_product(self):
+        pass
+
+    async def get_product(self):
+        pass
+
+    async def search_products(self):
+        pass
