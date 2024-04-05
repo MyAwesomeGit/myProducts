193a8be35e8deca77ba77494149fb29d4c17b174
Vasily Shuklin <b88bb@yandex.ru>
2024-04-05 18:44:31 +0500

    Add method test_get_product_fail

---
+++ tests/test_get_product_fail.py
@@ -0,0 +1,16 @@
+import pytest
+from src.controllers.product_manager import ProductManager
+
+products = {2: {"product_id": 123, "name": "Keyboard Keychron Q1", "category": "Electronics", "price": 112.99}}
+
+
+@pytest.mark.asyncio
+async def test_get_product_fail():
+    product_id = 99
+    product_manager = ProductManager(product_id=product_id,
+                                     products=products,
+                                     keyword=None,
+                                     category=None,
+                                     limit=None)
+    result = await product_manager.get_product()
+    assert result is None
