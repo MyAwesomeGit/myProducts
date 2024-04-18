0c663f151d2648bba79a2c5b0608084093122ac4
Vasily Shuklin <b88bb@yandex.ru>
2024-04-18 18:55:59 +0500

    Updated ProductsUserDB class

--- src/models/user_db.py
+++ src/models/user_db.py
@@ -2,6 +2,7 @@ from models.products_user import ProductsUser
 
 
 products_user_db = [
-    ProductsUser(username="test", password="123"),
-    ProductsUser(username="hello", password="111")
+    ProductsUser(username="test", password="123", access_permission="administrator"),
+    ProductsUser(username="hello", password="111", access_permission="guest"),
+    ProductsUser(username="iamuser", password="012", access_permission="user")
 ]
